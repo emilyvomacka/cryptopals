0380f838dcdf22b3f7afd6a3b1e67667207ec400
Brad Dettmer <11279144+redSlug@users.noreply.github.com>
2024-05-22 09:51:15 -0400

    add setup, README, requirements.txt for collaboration

---
+++ README.md
@@ -0,0 +1,14 @@
+# Cryptopals
+
+## Setup
+```bash
+python3.8 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+## Run
+```bash
+source venv/bin/activate
+python <file_name>
+```
--- s1c8.py
+++ s1c8.py
@@ -2,7 +2,27 @@
 # Deterministic: same input always produces same results
 # Stateless: you can do operations in any order, no state is kept. Functional??
 
+"""
+Detect AES in ECB mode
+In s1c8_input.txt there are a bunch of hex-encoded ciphertexts.
+
+One of them has been encrypted with ECB.
+
+Detect it.
+
+Remember that the problem with ECB is that it is stateless and deterministic;
+the same 16 byte plaintext block will always produce the same 16 byte ciphertext.
+
+"""
+
+from base64 import b64decode
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
 def decrypt_c8():
-  input_txt = open('set1/s1c8_input.txt', 'r').read()
+  input_txt = open('s1c8_input.txt', 'r').read()
   key = b'YELLOW SUBMARINE'
   decryptor = Cipher(algorithms.AES128(key), modes.ECB()).decryptor()
+
+
+print("hi")
+decrypt_c8()
