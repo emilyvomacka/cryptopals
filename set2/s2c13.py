7333ce2fbd3c41f4b356b205f57cefa896072cd0
Emily Vomacka <emilyvomacka@gmail.com>
2024-06-17 14:30:39 -0400

    neatening up c13

--- set2/s2c13.py
+++ set2/s2c13.py
@@ -19,7 +19,6 @@ def key_value_parse(input_string):
   for item in input_list:
     split = item.split("=")
     output_dict[split[0]] = split[1]
-  print(f'output is {output_dict}')
 
 key_value_parse("foo=bar&baz=qux&zap=zazzle")
 
@@ -64,37 +63,23 @@ assert ecb_decrypt(ecb_encrypt(b'somebody@email.com', key), key) == 'somebody@em
 # Encrypt the encoded user profile under the key; "provide" that to the "attacker".
 def create_encoded_profile(email):
   input = bytes(profile_for(email), 'utf-8')
-  print(f'profile string is {input}, len {len(input)}')
   return ecb_encrypt(input, key)
 
 profile_ciphertext = create_encoded_profile('foo@bar.com')
-print(f'encrypted profile: {profile_ciphertext}')
 # Decrypt the encoded user profile and parse it.
 decrypted_profile = ecb_decrypt(profile_ciphertext, key)
-print(f'decrypted profile: {decrypted_profile}')
 
 # Using only the user input to profile_for() (as an oracle to generate "valid" ciphertexts) and the ciphertexts themselves, make a role=admin profile.
 def make_admin_profile():
   # first get the encoding of "admin" in byte position 0-4 of a 16 byte block
   first_output = create_encoded_profile('12345678901234567890123456admin\v\v\v\v\v\v\v\v\v\v\v')
   admin_block = first_output[32:48]
-  print(f'admin block: {admin_block}')
 
-  print(f'admin block decrypted: {ecb_decrypt(first_output[32:], key)}')
 
   profile = bytearray(create_encoded_profile('tilapia@fishy'))
-  print(f'length before editing is {len(profile)}')
-  print(f'decryption which should be role=user: {ecb_decrypt(profile, key)}')
   edited_profile = profile[:32] + admin_block 
-  print(f'encrypted edited profile is {edited_profile}')
-  print(f'we expect this block to be admin: {edited_profile[32:37]}')
-  print(f'length after editing is {len(edited_profile)}')
-  for i in range(len(edited_profile)):
-    if edited_profile[i] != profile[i]:
-      print(f'byte {i} has been changed')
 
-  print(f'decryped profile first two blocks: {ecb_decrypt(edited_profile[:32], key)}')
-  print(f'decryped profile: {ecb_decrypt(edited_profile, key)}')
+  return ecb_decrypt(edited_profile, key)
 
 
-make_admin_profile()
+print(make_admin_profile())
